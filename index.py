78235bee231f8eafd304d29adaaed4e8cc521663
Vishal Verma <vishalvermadeveloper@gmail.com>
2019-12-29 01:23:11 +0530

    GPU Configs Added

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ import torchvision.datasets as datasets
 from torchvision.transforms import transforms
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
-from torchvision.utils import make_grid
+# from torchvision.utils import make_grid
 
 import torch.nn as nn
 
@@ -49,44 +49,59 @@ def show_transformed_image(image):
 
 train_size = int(0.8 * len(total_dataset))
 test_size = len(total_dataset) - train_size
-train_dataset, test_dataset = random_split(total_dataset, [train_size, test_size])
+train_dataset, test_dataset = random_split(
+    total_dataset,
+    [train_size, test_size]
+)
 
-train_dataset_loader = DataLoader(dataset= train_dataset, batch_size=100)
-test_dataset_loader = DataLoader(dataset = test_dataset, batch_size=1000)
+train_dataset_loader = DataLoader(
+    dataset=train_dataset,
+    batch_size=100
+)
+test_dataset_loader = DataLoader(dataset=test_dataset, batch_size=1000)
 
 
 class FlowerClassifierCNNModel(nn.Module):
-    
+
     def __init__(self, num_classes=5):
-        super(FlowerClassifierCNNModel,self).__init__()
-        
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=12, kernel_size=3,stride=1, padding=1)
+        super(FlowerClassifierCNNModel, self).__init__()
+
+        self.conv1 = nn.Conv2d(
+            in_channels=3, out_channels=12, kernel_size=3, stride=1, padding=1)
         self.relu1 = nn.ReLU()
-        
+
         self.maxpool1 = nn.MaxPool2d(kernel_size=2)
-        
-        self.conv2 = nn.Conv2d(in_channels=12, out_channels=24, kernel_size=3, stride=1, padding=1)
+
+        self.conv2 = nn.Conv2d(
+            in_channels=12,
+            out_channels=24,
+            kernel_size=3,
+            stride=1,
+            padding=1
+        )
         self.relu2 = nn.ReLU()
-        
+
         self.lf = nn.Linear(in_features=32 * 32 * 24, out_features=6)
-    
+
     def forward(self, input):
         output = self.conv1(input)
         output = self.relu1(output)
-        
+
         output = self.maxpool1(output)
-        
+
         output = self.conv2(output)
         output = self.relu2(output)
-        
+
         output = output.view(-1, 32 * 32 * 24)
 
         output = self.lf(output)
 
         return output
 
+
 if torch.cuda.is_available():
-    device = torch.device("cuda:0")  # you can continue going on here, like cuda:1 cuda:2....etc. 
+    # you can continue going on here, like cuda:1 cuda:2....etc.
+    device = torch.device("cuda:0")
     print("Running on the GPU")
 else:
     device = torch.device("cpu")
@@ -98,15 +113,16 @@ optimizer = Adam(cnn_model.parameters())
 loss_fn = nn.CrossEntropyLoss()
 
 
-
 def train_and_build(n_epoches):
     for epoch in range(n_epoches):
         cnn_model.train()
-        print( str(epoch) + " Epoch")
+        print(str(epoch) + " Epoch")
         for i, (images, labels) in enumerate(train_dataset_loader):
             optimizer.zero_grad()
-            outputs = cnn_model(images.to(device))
-            loss = loss_fn(outputs, labels.to(device))
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = cnn_model(images)
+            loss = loss_fn(outputs, labels)
             loss.backward()
             optimizer.step()
             # print(str(i) + "train nth")
@@ -117,6 +133,8 @@ train_and_build(200)
 cnn_model.eval()
 test_acc_count = 0
 for k, (test_images, test_labels) in enumerate(test_dataset_loader):
+    test_images = test_images.to(device)
+    test_labels = test_labels.to(device)
     test_outputs = cnn_model(test_images)
     _, prediction = torch.max(test_outputs.data, 1)
     test_acc_count += torch.sum(prediction == test_labels.data).item()
@@ -126,11 +144,11 @@ test_accuracy = test_acc_count / len(test_dataset)
 print(test_accuracy)
 
 
-test_image = Image.open(flowers_path+"/dandelion/13920113_f03e867ea7_m.jpg")
-test_image_tensor = transformations(test_image).float()
-test_image_tensor = test_image_tensor.unsqueeze_(0)
-output = cnn_model(test_image_tensor)
-class_index = output.data.numpy().argmax()
+# test_image = Image.open(flowers_path+"/dandelion/13920113_f03e867ea7_m.jpg")
+# test_image_tensor = transformations(test_image).float()
+# test_image_tensor = test_image_tensor.unsqueeze_(0)
+# output = cnn_model(test_image_tensor)
+# class_index = output.data.numpy().argmax()
 
-print(class_index)
-print(total_dataset.class_to_idx)
+# print(class_index)
+# print(total_dataset.class_to_idx)
